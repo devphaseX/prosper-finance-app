7ca3b69ed0d5e3b2c92552a3e5771e12e2c3a13d
Ayomide Lawal <ayomidelawal700@gmail.com>
2024-05-11 06:04:16 +0100

    add validation for required env variable

--- lib/env.ts
+++ lib/env.ts
@@ -1,6 +1,10 @@
 import { TypeOf, z } from 'zod';
 
-const envSchema = z.object({});
+const envSchema = z.object({
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(32),
+  CLERK_SECRET_KEY: z.string().min(32),
+  CLERK_PUBLISHABLE_KEY: z.string().min(32),
+});
 
 const env = envSchema.parse(process.env);
 
